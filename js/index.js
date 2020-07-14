496d59daed60946734733d746a4f784b7577eda1
vivianliu68 <vivianliu68@gmail.com>
2020-07-13 23:54:35 -0700

    New files
    New files

---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+
+  $(document).ready(function(){
+    $(".hide").click(function(){
+      $(".p1").slideUp(2000);
+    });
+  });
+
+  $(document).ready(function(){
+    $(".show").click(function(){
+      $(".p1").slidedown(2000);
+    });
+  });
+
+
